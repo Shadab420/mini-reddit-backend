728be013f44d152fadd3d5c387c73272eac1dc6e
Shadab420 <shadab_anwar@outlook.com>
2020-08-25 20:54:23 +0600

    added Post entity, migration, post insert and select all posts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
+import { Post } from './entities/post';
+import mikroORMConfig from './mikro-orm.config';
 
 
 const main = async () => {
-    const orm = MikroORM.init({
-        dbName: 'lireddit',
-        type: 'postgresql',
-        debug: !__prod__,
-        entities: [],
-    });
+    //connect to db
+    const orm = await MikroORM.init(mikroORMConfig);
+
+    //run migrations automatically
+    await orm.getMigrator().up();
+
+    //insert a post
+    // const post = await orm.em.create(Post, { title: "My second post" });
+    // await orm.em.persistAndFlush(post);
+
+    //select query
+    // const posts = await orm.em.find(Post, {});
+    // console.log(posts);
 }
+
+main().catch(err => console.log(err));
 
 
